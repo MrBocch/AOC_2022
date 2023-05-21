afda2aaa9772480227e7c9a65d0e3b68473e2bdb
MrBochh <jorgealberto1436@gmail.com>
2023-05-21 01:05:51 -0600

    Adding existing to repo

---
+++ day1.lua
@@ -0,0 +1,33 @@
+file = io.open("input1.txt", "r")
+io.input(file)
+
+max = {0, 0, 0}
+i = 0
+while true do
+    local line = io.read("*line")
+    if line == nil then break end
+
+    if line == "" then
+        for k=1, #max do
+            if max[k] < i then
+                max[k] = i
+                break
+            end
+        end
+        i = 0
+    else
+        i = i + tonumber(line)
+    end
+end
+
+print(max[1])
+--answer is 66306
+
+--second part
+sum = 0
+for k=1, #max do
+    sum = sum + max[k]
+end
+print(sum)
+
+--answer is 195292
