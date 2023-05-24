d6d55c743201fdad98279952c50bedc28465ff67
MrBochh <jorgealberto1436@gmail.com>
2023-05-23 22:45:51 -0600

    I have to delete everything everything is wrong

--- day4.lua
+++ day4.lua
@@ -4,34 +4,43 @@ function part1()
     io.input(file)
 
     local contained = 0
+    local nums = {}
+    local i = 1
 
     while true do
         local line = io.read("*line")
         if line == nil then break end
 
-        local nums = {}
-        local i = 1
         for v in string.gmatch( line, "(%d+)") do
             nums[i] = v
             i = i + 1
         end
 
-        for i=1, #nums, 4 do
-            num1 = nums[i]
-            num2 = nums[i+1]
-            num3 = nums[i+2]
-            num4 = nums[i+3]
-
-            if (num1 <= num3 and num2 >= num4) or (num3 <= num1 and num4 >= num2) then
-                contained = contained +1
-                print(num1.."-"..num2.." , "..num3.."-"..num4)
-            end
-        end
-
     end
 
-    print("Part1: "..contained)
-    --569 too high
+    for i=1, #nums, 4 do
+        local a1 = nums[i]
+        local a2 = nums[i+1]
+
+        local b1 = nums[i+2]
+        local b2 = nums[i+3]
+
+        --[[
+            I cant understand why does this not work?
+        if start1 >= start2 and end1 <= end2 then
+            contained = contained + 1
+
+        elseif start1 <= start2 and end1 >= end2 then
+            contained = contained + 1
+        end
+        ]]
+        if (a1 <= b1 and a2 >= b2) or (b1 <= a1 and b2 >= a2) then
+            contained = contained+ 1
+        end
+    end
+
+    print("Lua Part1: "..contained)
+    --Correct 542 
 end
 
 part1()
