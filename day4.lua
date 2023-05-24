0f597afe23be909ff95d2c443706dbabc000f4ca
MrBochh <jorgealberto1436@gmail.com>
2023-05-23 23:14:04 -0600

    This is wrong but i dont know why

--- day4.lua
+++ day4.lua
@@ -29,8 +29,43 @@ function part1()
     end
 
 
-    print("Lua Part1: "..contained)
+    print("Part1: "..contained)
     --Correct 542 
 end
 
+function part2()
+    local file = io.open("input4.txt", "r")
+    io.input(file)
+
+    local overlaps = 0
+    local nums = {}
+
+    while true do
+        local line = io.read("*line")
+        if line == nil then break end
+
+        -- I did not knew about pattern matching functions
+        start1, end1, start2, end2 = string.match(line, "(%d+)-(%d+),(%d+)-(%d+)")
+        
+        start1 = tonumber(start1)
+        end1 = tonumber(end1)
+
+        start2 = tonumber(start2)
+        end2= tonumber(end2)
+
+        if end1 >= start2 then
+            overlaps = overlaps + 1
+            print(start1.."-"..end1..","..start2.."-"..end2)
+        elseif end2 >= start1 then
+            overlaps = overlaps + 1
+        end
+
+    end
+
+
+    print("Part2: "..overlaps)
+    --1000 to high
+end
+
 part1()
+part2()
