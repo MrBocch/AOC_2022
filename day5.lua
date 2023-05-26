2ec60546534374ce8b2ccb49396e0d6618d7a2a8
MrBochh <jorgealberto1436@gmail.com>
2023-05-26 14:54:04 -0600

    Part 1 solution for day 5

---
+++ day5.lua
@@ -0,0 +1,104 @@
+stacks = {
+    {"B","Z","T"},
+    {"V","H","T","D","N"},
+    {"B","F","M","D"},
+    {"T","J","G","W","V","Q","L"},
+    {"W","D","G","P","V","F","Q","M"},
+    {"V","Z","Q","G","H","F","S"},
+    {"Z","S","N","R","L","T","C","W"},
+    {"Z","H","W","D","J","N","R","M"},
+    {"M","Q","L","F","D","S"}
+}
+
+function part1()
+    local file = io.open("input5.txt","r")
+    io.input(file)
+
+    for i=1, 10 do --skips over the stacks
+        io.read("*line")
+    end
+
+    while true do
+        local line = io.read("*line")
+        if line == nil then break end
+                                                --("%d+") becuse it might be more than 9
+        crates, pile1, pile2 = string.match(line, "move (%d+) from (%d+) to (%d+)")
+        local crates = tonumber(crates)
+        local pile1 = tonumber(pile1)
+        local pile2 = tonumber(pile2)
+
+        --print(#stacks[pile1])  length of pile1
+        --print(stacks[pile1][#stacks[pile1]]) what value
+        
+        --print(#stacks[pile2]) length of pile2
+        --print(stacks[pile2][#stacks[pile2]]) what value 
+
+        for i=1, crates do
+            table.insert(stacks[pile2], stacks[pile1][#stacks[pile1]])
+            table.remove(stacks[pile1], #stacks[pile1])
+        end
+
+    end
+
+    io.write("Part 1: ")
+    for i=1, 9 do
+        io.write(stacks[i][#stacks[i]])
+    end
+end
+
+part1()
+
+
+
+
+--[[ playground code 
+print("--------")
+
+x1 = {"Z", "N"}
+x2 = {"M","C","D"}
+x3 = {"P"}
+
+while false do
+
+    for i=1, 1 do
+        table.insert(x1, x2[#x2])
+        table.remove(x2, #x2)
+    end
+
+    for i=1, 3 do
+        table.insert(x3, x1[#x1])
+        table.remove(x1, #x1)
+    end
+
+    for i=1, 2 do
+        table.insert(x1, x2[#x2])
+        table.remove(x2, #x2)
+    end
+    for i=1, 1 do
+        table.insert(x2, x1[#x1])
+        table.remove(x1, #x1)
+    end
+    break
+end
+
+for i=1, #x1 do
+    io.write(x1[i].." ")
+end
+print()
+
+for i=1, #x2 do
+    io.write(x2[i].." ")
+end
+
+print()
+for i=1, #x3 do
+    io.write(x3[i].." ")
+end
+print()
+]]
+
+--matrix = {{1,2,3, 10}, {4,5,6, 10, 50}, {7,8,9, 10,10,90}}
+--print(matrix[1][#matrix[1]])
+--print(matrix[3][#matrix[3]])
+
+
